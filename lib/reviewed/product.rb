7088975e581f46a7da78b517a86d60d0cc3af007
Nick Plante <nap@zerosum.org>
2012-09-07 12:08:43 -0400

    tag filtering for product attachments (like articles)

--- lib/reviewed/product.rb
+++ lib/reviewed/product.rb
@@ -1,4 +1,13 @@
 module Reviewed
   class Product < Base
+    def attachments(tag=nil)
+      if tag.present?
+        @attributes[:attachments].select do |attachment|
+          attachment.tags.map(&:downcase).include?(tag.downcase)
+        end
+      else
+        @attributes[:attachments]
+      end
+    end
   end
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,4 +1,23 @@
 require 'spec_helper'
 
 describe Reviewed::Product do
+  describe 'attachments' do
+    use_vcr_cassette 'product/attachments'
+
+    before(:each) do
+      @product= Reviewed::Product.find('medium-green-charcoal')
+    end
+
+    it 'returns all attachments' do
+      @product.attachments.length.should > 1
+    end
+
+    it 'finds attachments by tag' do
+      attachments = @product.attachments('hero')
+      attachments.length.should == 1
+      attachments.each do |attachment|
+        attachment.tags.join(',').should match(/hero/i)
+      end
+    end
+  end
 end
