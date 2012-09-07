343b0857b680c98ca475e6b6c4b8e5aaec82831d
Nick Plante <nap@zerosum.org>
2012-09-07 11:27:03 -0400

    optionally filter article attachments by tag
    had to mock up fixture data for this

--- lib/reviewed/article.rb
+++ lib/reviewed/article.rb
@@ -3,5 +3,15 @@ module Reviewed
     def find_page(slug)
       pages.find { |page| page.slug.match(/#{slug}/i) }
     end
+
+    def attachments(tag=nil)
+      if tag.present?
+        @attributes[:attachments].select do |attachment|
+          attachment.tags.include?(tag)
+        end
+      else
+        @attributes[:attachments]
+      end
+    end
   end
 end
--- spec/article_spec.rb
+++ spec/article_spec.rb
@@ -10,4 +10,24 @@ describe Reviewed::Article do
       article.find_page('performance').should == article.pages[1]
     end
   end
+
+  describe 'attachments' do
+    use_vcr_cassette 'article/attachments'
+
+    before(:each) do
+      @article = Reviewed::Article.find('big-green-egg-medium-charcoal-grill-review')
+    end
+
+    it 'returns all attachments' do
+      @article.attachments.length.should > 1
+    end
+
+    it 'finds attachments by tag' do
+      attachments = @article.attachments('Hero')
+      attachments.length.should == 1
+      attachments.each do |attachment|
+        attachment.tags.should include('Hero')
+      end
+    end
+  end
 end
