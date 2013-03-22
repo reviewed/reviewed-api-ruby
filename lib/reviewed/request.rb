84e1a09dc5f49daf0010a87d850fc2cad7b57623
Sam Beam <sbeam@onsetcorps.net>
2013-03-22 17:27:36 -0400

    CGI::escape search terms passed to api #920

--- lib/reviewed/request.rb
+++ lib/reviewed/request.rb
@@ -16,7 +16,8 @@ module Reviewed
 
     # Get request on resource#show
     def find(id, params={})
-      object_from_response(:get, "#{path}/#{id}", params)
+      url_path = [path, CGI::escape(id.to_s)]
+      object_from_response(:get, url_path.join('/'), params)
     end
 
     # Get request on resource#index with query params
