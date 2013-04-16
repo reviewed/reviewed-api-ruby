f84e1559218bc907176d6e4eeff20265469d8375
Sam Beam <sbeam@onsetcorps.net>
2013-04-16 14:32:07 -0400

    serialize with syck, otherwise getting binary vcr bodies here

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,13 +14,14 @@ RSpec.configure do |config|
 end
 
 VCR.configure do |config|
-  config.default_cassette_options = { record: :new_episodes }
+  config.ignore_localhost = false
+  config.default_cassette_options = { :record => :new_episodes, :serialize_with => :syck }
   config.cassette_library_dir = 'spec/fixtures/vcr'
-  config.hook_into :webmock
+  config.hook_into :faraday
   config.configure_rspec_metadata!
 end
 
 
-TEST_URL = 'https://the-guide-staging.herokuapp.com/api/v1'
+TEST_URL = 'http://localhost:3000/api/v1'
 TEST_KEY = ENV['REVIEWED_API_KEY']
 
