5932ca156c7dd37f779bdee9ad522b76e9d68e79
Sam Beam <sbeam@onsetcorps.net>
2013-07-24 15:55:10 -0400

    refactor tests to use mocks/stubs for api and cache store
    get rid of singleton to improve testability

--- lib/reviewed/cache.rb
+++ lib/reviewed/cache.rb
@@ -2,14 +2,9 @@ require 'active_support/cache'
 
 module Reviewed
   class Cache
-    class << self
-      def new(*args)
-        @instance ||= super(*args)
-      end
 
-      def store
-        @store ||= ActiveSupport::Cache.lookup_store(:redis_store, ENV["REDISCLOUD_URL"], {expires_in: 90.minutes})
-      end
+    def self.store
+      @store ||= ActiveSupport::Cache.lookup_store(:redis_store, ENV["REDISCLOUD_URL"], {expires_in: 90.minutes})
     end
 
     def initialize(app)
@@ -17,39 +12,36 @@ module Reviewed
     end
 
     def call(env)
-      store_response = false
-      serve_from_cache = true
+      @url = env[:url]
 
-      key = env[:url].dup
-      key.query = nil
-
-      unless self.class.store.exist?(key.to_s)
-        store_response = true
-      end
-
-      if env[:url].query
-        if env[:url].query.match /reset-cache/
-          store_response = true
-          serve_from_cache = false
-        elsif env[:url].query.match /skip-cache/
-          store_response = false
-          serve_from_cache = false
-        end
-      end
-
-      if serve_from_cache && self.class.store.exist?(key.to_s)
-        Hashie::Mash.new(Marshal.load( self.class.store.read(key.to_s) ))
+      if serve_from_cache && self.class.store.exist?(cache_key)
+        Hashie::Mash.new(Marshal.load( self.class.store.read(cache_key) ))
       else
-        @app.call(env).on_complete do |resp|
+        @app.call(env).on_complete do |response|
           if store_response
-            self.class.store.delete(key.to_s)
-            self.class.store.write(key.to_s, Marshal.dump(resp))
-            key.to_s
+            self.class.store.delete(cache_key)
+            self.class.store.write(cache_key, Marshal.dump(response))
+            cache_key
           else
-            resp
+            response
           end
         end
       end
     end
+
+    private
+
+    def serve_from_cache
+      @url.query.blank? || !@url.query.match(/\b(skip|reset)-cache\b/)
+    end
+
+    def store_response
+      @url.query.blank? || !@url.query.match(/\bskip-cache\b/)
+    end
+
+    def cache_key
+      @url.request_uri
+    end
+
   end
 end
--- spec/cache_spec.rb
+++ spec/cache_spec.rb
@@ -1,64 +1,58 @@
 require 'spec_helper'
 
-def get(path, params = {})
-  conn.get(path, params) do |req|
-    req.headers['X-Reviewed-Authorization'] = TEST_KEY
-  end
-end
 
-describe Reviewed::Cache, vcr: true do
-  let (:stubs) do
-    Faraday::Adapter::Test::Stubs.new do |stub|
-      stub.get('http://localhost:3000/api/v1/articles') { [200, {}, 'I like turtles'] }
-      stub.get('http://localhost:3000/api/v1/products') { [200, {}, 'Weee products'] }
-    end
-  end
+describe Reviewed::Cache do
 
-  let(:conn) do
-    Faraday.new do |builder|
+  describe 'call' do
+
+    conn = Faraday.new do |builder|
       builder.use Reviewed::Cache
-      builder.adapter :test, stubs
-    end
-  end
-
-  before(:each) do
-    Reviewed::Cache.store.clear
-  end
-
-  it "caches anything that doesn't have skip-cache or reset-cache" do
-    get('http://localhost:3000/api/v1/articles')
-    Reviewed::Cache.store.read('http://localhost:3000/api/v1/articles').should_not be_nil
-  end
-
-  it "can cache more than one thing" do
-    get('http://localhost:3000/api/v1/articles')
-    get('http://localhost:3000/api/v1/products')
-    Reviewed::Cache.store.read('http://localhost:3000/api/v1/articles').should_not be_nil
-    Reviewed::Cache.store.read('http://localhost:3000/api/v1/products').should_not be_nil
-  end
-
-  it "always returns the same instance" do
-    numero_uno = Reviewed::Cache.new("pretend this string is actually an app")
-    numero_dos = Reviewed::Cache.new("pretend this string is actually an app")
-    numero_uno.object_id.should eq(numero_dos.object_id)
-  end
-
-  it "serves responses from the cache when fresh and does not call the app" do
-    Reviewed::Cache.store.write('http://localhost:3000/api/v1/articles', Marshal.dump(:body => "old response"))
-    resp = get 'http://localhost:3000/api/v1/articles'
-    resp[:body].should eq("old response")
-  end
-
-  describe "cache busting and skipping" do
-    it "does not cache responses with skip-cache as a query param" do
-      get 'http://localhost:3000/api/v1/articles', {:"skip-cache" => true}
-      Reviewed::Cache.store.read('http://localhost:3000/api/v1/articles').should be_nil
+      builder.adapter :test, Faraday::Adapter::Test::Stubs.new do |stub|
+        stub.get('/articles') { [200, {}, 'I like turtles'] }
+        stub.get('/products') { [200, {}, 'Weee products'] }
+      end
     end
 
-    it "replaces cached content with app response when reset-cache is a query param" do
-      Reviewed::Cache.store.write('http://localhost:3000/api/v1/articles', "old response")
-      get 'http://localhost:3000/api/v1/articles', {:"reset-cache" => true}
-      Reviewed::Cache.store.read('http://localhost:3000/api/v1/articles').should_not eq("old response")
+    conn.headers = { "X-Reviewed-Authorization" => TEST_KEY }
+
+    it "caches anything that doesn't have skip-cache or reset-cache" do
+      mock_cache_val = mock
+      Marshal.stub(:dump).with(hash_including(:body => 'I like turtles')).and_return(mock_cache_val)
+      Reviewed::Cache.store.should_receive(:write).with('/articles', mock_cache_val)
+      conn.get('/articles')
     end
+
+    it "can cache more than one thing" do
+      mock_cache_val = mock
+      Marshal.stub(:dump => mock_cache_val)
+      Reviewed::Cache.store.should_receive(:write).with('/articles', mock_cache_val)
+      Reviewed::Cache.store.should_receive(:write).with('/products', mock_cache_val)
+
+      conn.get('/articles')
+      conn.get('/products')
+    end
+
+    it "serves responses from the cache when fresh and does not call the app" do
+      marshalled_response = Marshal.dump(body: 'old musty response')
+      Reviewed::Cache.store.stub(:exist? => true)
+      Reviewed::Cache.store.should_receive(:read).with('/articles').and_return(marshalled_response)
+      resp = conn.get '/articles'
+      resp[:body].should eq("old musty response")
+    end
+
+    describe "cache busting and skipping" do
+      it "does not cache responses with skip-cache as a query param" do
+        Reviewed::Cache.store.should_not_receive(:read)
+        Reviewed::Cache.store.should_not_receive(:write)
+        conn.get '/articles', {:"skip-cache" => true}
+      end
+
+      it "replaces cached content with app response when reset-cache is a query param" do
+        Reviewed::Cache.store.should_not_receive(:read)
+        Reviewed::Cache.store.should_receive(:write)
+        conn.get '/articles', {:"reset-cache" => true}
+      end
+    end
+
   end
 end
